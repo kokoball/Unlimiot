452992214dc9921a11bca7e4ad37bc6c44f078ff
DESKTOP-00QSB4T\ko <kbp4154@naver.com>
2021-08-14 14:24:20 +0900

    Frontend first commit

---
+++ frontend/App.tsx
@@ -0,0 +1,21 @@
+import 'react-native-gesture-handler';
+import React from 'react';
+import {enableScreens} from 'react-native-screens';
+import {SafeAreaProvider} from 'react-native-safe-area-context';
+import { NavigationContainer, DefaultTheme, DarkTheme } from '@react-navigation/native';
+import { AppearanceProvider, useColorScheme } from 'react-native-appearance';
+import MainNavigator from './src/screens/MainNavigator';
+
+enableScreens();
+
+export default function App() {
+  return (
+    <AppearanceProvider>
+      <SafeAreaProvider>
+        <NavigationContainer>
+          <MainNavigator />
+        </NavigationContainer>
+      </SafeAreaProvider>
+    </AppearanceProvider>
+  );
+}
