ab28a027a23f9f339f30ee4f7529df170ede1254
Jin <rifampicin@naver.com>
2021-08-18 15:42:04 +0000

    :card_file_box:

--- backend/unlimiot_be/unlimiot_be/settings.py
+++ backend/unlimiot_be/unlimiot_be/settings.py
@@ -78,24 +78,30 @@ WSGI_APPLICATION = 'unlimiot_be.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': str(BASE_DIR / 'db.sqlite3'),
+if os.getcwd() == '/home/ubuntu/www2/Unlimiot/backend/unlimiot_be/unlimiot_be':
+    DATABASES = {
+   'default': {
+       'ENGINE': 'django.db.backends.postgresql',
+       'NAME': 'unlimiot',
+       'USER': 'jinjin',
+       'PASSWORD': os.environ.get('DB_PASS'),
+       'HOST': os.environ.get('HOST'),
+       'PORT': '5432',
+       }
     }
-}
+    print('Integrating AWS EC2 with AWS RDS')
+    
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': str(BASE_DIR / 'db.sqlite3'),
+        }
+    }
+    print('Test run')
 
 
-# DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.postgresql',
-#        'NAME': 'net_unlimiot',
-#        'USER': 'jinjin',
-#        'PASSWORD': os.environ.get('DB_PASS'),
-#        'HOST': os.environ.get('HOST'),
-#        'PORT': '5432',
-#    }
-# }
+
 
 
 
