cb272fbb8013c4202a3ca1faad1808b6cb53b4ba
Jin <rifampicin@naver.com>
2021-08-18 14:24:07 +0000

    :see_no_evil:git ignore file

--- backend/unlimiot_be/unlimiot_be/settings.py
+++ backend/unlimiot_be/unlimiot_be/settings.py
@@ -125,6 +125,7 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [
     BASE_DIR / 'static',               
 ]
+#
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
