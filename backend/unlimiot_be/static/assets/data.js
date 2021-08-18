b07a805fbed3ae16d31705fc678eadbee34cac9a
Jin <rifampicin@naver.com>
2021-08-18 10:28:44 +0000

    :monocle_face: data update

--- backend/unlimiot_be/static/assets/data.js
+++ backend/unlimiot_be/static/assets/data.js
@@ -13,7 +13,7 @@ var data = [
       img: '<img src="{% static "assets/images/restaurant/restaurant1.png" %}" >'
     },{
       title: '벳노니의 농장',
-      partnership: '2잔 이상 구매시 10% 할인',
+      partnership: '만원이상 결제시 5% 할인',
       lat: '35.829422111357644',
       lng: '127.08241952548886',
       img: '<img src="{% static "assets/images/restaurant/restaurant1.png" %}" >'
@@ -24,175 +24,175 @@ var data = [
       lng: '127.09605364060548',
       img: '<img src="{% static "assets/images/restaurant/restaurant1.png" %}" >'
     },{
-      title: '카페지인',
-      partnership: '2잔 이상 구매시 10% 할인',
+      title: '선일농원',
+      partnership: '만원이상 결제시 5% 할인',
       lat: '34.83686739913989',
       lng: '126.12305337058872',
       img: '<img src="{% static "assets/images/restaurant/restaurant1.png" %}" >'
     },{
-      title: '카페지인',
+      title: '보라매농장',
       partnership: '2잔 이상 구매시 10% 할인',
       lat: '34.7091954272817',
       lng: '125.92672218009464',
       img: '<img src="{% static "assets/images/restaurant/restaurant1.png" %}" >'
     },{
-      title: '카페지인3',
-      partnership: '2잔 이상 구매시 10% 할인',
+      title: '다육농장',
+      partnership: '만원이상 결제시 5% 할인',
       lat: '34.691260753326894',
       lng: '126.90565134080038',
       img: '<img src="{% static "assets/images/restaurant/restaurant1.png" %}" >'
     },{
-      title: '카페지인',
+      title: '휘인지인 농장',
       partnership: '2잔 이상 구매시 10% 할인',
       lat: '35.64271889751915',
       lng: '126.60375442047945',
       img: '<img src="{% static "assets/images/restaurant/restaurant1.png" %}" >'
     },{
-      title: '카페지인',
-      partnership: '2잔 이상 구매시 10% 할인',
+      title: '줄리스 팜',
+      partnership: '네트앱 유저 10% 할인',
       lat: '35.25157445633953',
       lng: '127.22434280551657',
       img: '<img src="{% static "assets/images/restaurant/restaurant1.png" %}" >'
     },{
-      title: '카페지인4',
-      partnership: '2잔 이상 구매시 10% 할인',
+      title: '마이티 팜',
+      partnership: '네트앱 유저 10% 할인',
       lat: '34.75930733047455',
       lng: '127.19168025486954',
       img: '<img src="{% static "assets/images/restaurant/restaurant1.png" %}" >'
     },{
-      title: '카페지인',
+      title: '다육이 농장',
       partnership: '2잔 이상 구매시 10% 할인',
       lat: '35.07534116267619',
       lng: '126.76380089465971',
       img: '<img src="{% static "assets/images/restaurant/restaurant1.png" %}" >'
     },{
-      title: '카페지인',
-      partnership: '2잔 이상 구매시 10% 할인',
+      title: '중광농원',
+      partnership: '네트앱 유저 10% 할인',
       lat: '34.82636520328515',
       lng: '127.01856875307746',
       img: '<img src="{% static "assets/images/restaurant/restaurant1.png" %}" >'
     },{
-      title: '카페지인5',
-      partnership: '2잔 이상 구매시 10% 할인',
+      title: '백기농장',
+      partnership: '네트앱 유저 10% 할인',
       lat: '34.79686645699486',
       lng: '127.6685534300374',
       img: '<img src="{% static "assets/images/restaurant/restaurant1.png" %}" >'
     },{
-      title: '카페지인',
-      partnership: '2잔 이상 구매시 10% 할인',
+      title: '파이루 농장',
+      partnership: '네트앱 유저 10% 할인',
       lat: '35.43186110679673',
       lng: '127.42546488702531',
       img: '<img src="{% static "assets/images/restaurant/restaurant1.png" %}" >'
     },{
-      title: '카페지인',
+      title: '순이네 집',
       partnership: '2잔 이상 구매시 10% 할인',
       lat: '35.259171947378334',
       lng: '126.55585001250141',
       img: '<img src="{% static "assets/images/restaurant/restaurant1.png" %}" >'
     },{
-      title: '카페지인',
+      title: '샘체 체험농장',
       partnership: '2잔 이상 구매시 10% 할인',
       lat: '36.06740797427603',
       lng: '126.71520352121836',
       img: '<img src="{% static "assets/images/restaurant/restaurant1.png" %}" >'
     },{
-      title: '카페지인6',
-      partnership: '2잔 이상 구매시 10% 할인',
+      title: '다다연 체험농장',
+      partnership: '네트앱 유저 10% 할인',
       lat: '35.94402240807474',
       lng: '126.72658591474031',
       img: '<img src="{% static "assets/images/restaurant/restaurant1.png" %}" >'
     },{
-      title: '카페지인',
-      partnership: '2잔 이상 구매시 10% 할인',
+      title: '정일체험농원',
+      partnership: '네트앱 유저 10% 할인',
       lat: '34.57713975859865',
       lng: '126.5800208697143',
       img: '<img src="{% static "assets/images/restaurant/restaurant1.png" %}" >'
     },{
-      title: '카페지인',
-      partnership: '2잔 이상 구매시 10% 할인',
+      title: '시은체험농장',
+      partnership: '네트앱 유저 10% 할인',
       lat: '34.56444692362574',
       lng: '126.8459239306881',
       img: '<img src="{% static "assets/images/restaurant/restaurant1.png" %}" >'
     },{
-      title: '카페지인',
+      title: '팜팜주말농장',
       partnership: '2잔 이상 구매시 10% 할인',
       lat: '34.371277478982535',
       lng: '126.71258703344617',
       img: '<img src="{% static "assets/images/restaurant/restaurant1.png" %}" >'
     },{
-      title: '카페지인7',
-      partnership: '2잔 이상 구매시 10% 할인',
+      title: '유미니 주말농원',
+      partnership: '네트앱 유저 10% 할인',
       lat: '34.446858200198726',
       lng: '126.19372564937075',
       img: '<img src="{% static "assets/images/restaurant/restaurant1.png" %}" >'
     },{
-      title: '카페지인',
-      partnership: '2잔 이상 구매시 10% 할인',
+      title: '하늘공원주말농장',
+      partnership: '네트앱 유저 10% 할인',
       lat: '34.99460502258792',
       lng: '127.38461298120744',
       img: '<img src="{% static "assets/images/restaurant/restaurant1.png" %}" >'
     },{
-      title: '카페지인',
-      partnership: '2잔 이상 구매시 10% 할인',
+      title: '파인팜주말농원',
+      partnership: '네트앱 유저 10% 할인',
       lat: '35.59275531757073',
       lng: '126.8985551814836',
       img: '<img src="{% static "assets/images/restaurant/restaurant1.png" %}" >'
     },{
-      title: '카페지인',
+      title: '하와이체험농장 ',
       partnership: '2잔 이상 구매시 10% 할인',
       lat: '35.23346965466413',
       lng: '127.50538593829634',
       img: '<img src="{% static "assets/images/restaurant/restaurant1.png" %}" >'
     },{
-      title: '카페지인8',
-      partnership: '2잔 이상 구매시 10% 할인',
+      title: '사인 포도농장',
+      partnership: '네트앱 유저 10% 할인',
       lat: '35.12927387869291',
       lng: '127.78083986152764',
       img: '<img src="{% static "assets/images/restaurant/restaurant1.png" %}" >'
     },{
-      title: '카페지인',
-      partnership: '2잔 이상 구매시 10% 할인',
+      title: '민지네 체험농원',
+      partnership: '네트앱 유저 10% 할인',
       lat: '35.108612170622166',
       lng: '126.28499015975432',
       img: '<img src="{% static "assets/images/restaurant/restaurant1.png" %}" >'
     },{
-      title: '카페지인',
-      partnership: '2잔 이상 구매시 10% 할인',
+      title: '윤이네 농원&카페',
+      partnership: '네트앱 유저 10% 할인',
       lat: '34.813537746391724',
       lng: '126.38555605486796',
       img: '<img src="{% static "assets/images/restaurant/restaurant1.png" %}" >'
     },{
-      title: '카페지인',
+      title: '송인과수원',
       partnership: '2잔 이상 구매시 10% 할인',
       lat: '36.3262708171871',
       lng: '126.58858724670783',
       img: '<img src="{% static "assets/images/restaurant/restaurant1.png" %}" >'
     },{
-      title: '카페지인9',
-      partnership: '2잔 이상 구매시 10% 할인',
+      title: '파필 여명농장',
+      partnership: '네트앱 유저 10% 할인',
       lat: '36.121670141032084',
       lng: '126.95857717325826',
       img: '<img src="{% static "assets/images/restaurant/restaurant1.png" %}" >'
     },{
-      title: '카페지인',
-      partnership: '2잔 이상 구매시 10% 할인',
+      title: '체험 솔바위 농장',
+      partnership: '네트앱 유저 10% 할인',
       lat: '35.70571835165165',
       lng: '127.55643279108507',
       img: '<img src="{% static "assets/images/restaurant/restaurant1.png" %}" >'
     },{
-      title: '카페지인',
-      partnership: '2잔 이상 구매시 10% 할인',
+      title: '서랑 야생농원',
+      partnership: '네트앱 유저 10% 할인',
       lat: '35.28284993563477',
       lng: '126.99749937841862',
       img: '<img src="{% static "assets/images/restaurant/restaurant1.png" %}" >'
     },{
-      title: '카페지인',
-      partnership: '2잔 이상 구매시 10% 할인',
+      title: '시민체험행복텃밭',
+      partnership: '네트앱 유저 10% 할인',
       lat: '34.927771529671745',
       lng: '126.64871835741985',
       img: '<img src="{% static "assets/images/restaurant/restaurant1.png" %}" >'
     },{
-      title: '카페지인9',
+      title: '영월농장',
       partnership: '2잔 이상 구매시 10% 할인',
       lat: '34.76773434889422',
       lng: '126.72012948682482',
